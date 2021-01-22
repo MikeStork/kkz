e76716c9f991708b2d2473ce5eaaab95a324a7bd
MikeStork <44142875+MikeStork@users.noreply.github.com>
2021-01-22 19:17:56 +0100

    Dodaj pliki projektów.

---
+++ program1/program1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] tab = new int[10][];
+            for(int i = 0; i < tab.GetLength(0); i++)
+            {
+                tab[i] = new int[10];
+            }
+            for (int j = 0; j < 10; j++)
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    tab[i][j] = (i + 1) * (j + 1);
+                }
+            }
+            for (int j = 0; j < tab.Length; j++)
+            {
+                for (int i = 0; i < tab.Length; i++)
+                {
+                    Console.Write(tab[j][i] + "\t");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
