5c893dc958122324b4382bc515a2357883879abb
MikeStork <44142875+MikeStork@users.noreply.github.com>
2021-01-23 13:21:46 +0100

    asdasda

--- program1/program1/Program.cs
+++ program1/program1/Program.cs
@@ -6,25 +6,73 @@ namespace program1
     {
         static void Main(string[] args)
         {
+            //z1
+            string rl = Console.ReadLine();
+            int liczba = Int32.Parse(rl);
+            //liczba = Convert.ToInt32(rl);
+            string newFloat = Console.ReadLine();
+            float nowyFloat = (float)Convert.ToDouble(newFloat);
+            //z2
+            for (int i = 1; i<=10; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            int j = 1;
+            while (j <= 10)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            //z3
+            int[,] tab2d = new int[,] { { 1, 2, 3 }, { 2, 4, 6 }, { 3, 6, 9 } };
+            for (int i = 0; i < tab2d.GetLength(0); i++)
+            {
+                for (int ii = 0; ii < tab2d.GetLength(1); ii++)
+                    Console.Write(tab2d[i, ii] + "\t");
+                Console.WriteLine("");
+            }
+            //z4
+            int[][] tab2dJ = new int[2][];
+            tab2dJ[0] = new int[] { 5, 69, 5, -45 };
+            tab2dJ[1] = new int[] { -9, 9};
+            foreach(int [] tmpTab in tab2dJ)
+            {
+                foreach(int tmp in tmpTab)
+                {
+                    Console.WriteLine(tmp);
+                }
+            }
+            //z5
+            int[,] tabliczka = new int[10, 10];
+            for (int i = 0; i < tabliczka.GetLength(0); i++)
+            {
+                for(int ii = 0; ii< tabliczka.GetLength(1);ii++)
+                    tabliczka[i, ii] = (i + 1) * (ii + 1);
+            }
+
+
+            //ver 2
             int[][] tab = new int[10][];
-            for(int i = 0; i < tab.GetLength(0); i++)
+            for (int i = 0; i < tab.GetLength(0); i++)
             {
                 tab[i] = new int[10];
             }
-            for (int j = 0; j < 10; j++)
+            for (int jj = 0; jj < 10; jj++)
             {
-                for(int i = 0; i < 10; i++)
+                for (int i = 0; i < 10; i++)
                 {
-                    tab[i][j] = (i + 1) * (j + 1);
+                    tab[i][jj] = (i + 1) * (jj + 1);
                 }
             }
-            for (int j = 0; j < tab.Length; j++)
+
+            for (int jjj = 0; jjj < tab.Length; jjj++)
             {
                 for (int i = 0; i < tab.Length; i++)
                 {
-                    Console.Write(tab[j][i] + "\t");
+                    Console.Write(tab[jjj][i] + "\t");
                 }
-                Console.WriteLine(""); 
+                Console.WriteLine("");
             }
         }
     }
---
+++ program2/program2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace program2
+{
+    class Program
+    {
+        private static void Wrapper()
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+            //string stringer = "Essa";
+            //Console.WriteLine($"Ale mam dzisiaj fajnego stringa: {stringer}");
+            //int num;
+
+            //intro starwars start
+            Console.Beep(294, 100);
+            Console.Beep(294, 100);
+            Console.Beep(294, 100);
+            Console.Beep(391, 1000);
+            Console.Beep(587, 1000);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(783, 1000);
+            Console.Beep(587, 500);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(783, 1000);
+            Console.Beep(587, 500);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(523, 100);
+            Console.Beep(440, 500);
+            Console.Beep(37, 50);
+            Console.Beep(294, 100);
+            Console.Beep(294, 50);
+            Console.Beep(391, 1000);
+            Console.Beep(587, 1000);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(783, 1000);
+            Console.Beep(587, 500);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(783, 1000);
+            Console.Beep(587, 500);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(523, 100);
+            Console.Beep(440, 500);
+            Console.Beep(37,50);
+            Console.Beep(294, 100);
+            Console.Beep(294, 50);
+            Console.Beep(329, 750);
+            Console.Beep(329, 250);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(392, 100);
+            Console.Beep(392, 100);
+            Console.Beep(440, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(329, 250);
+            Console.Beep(369, 500);
+            Console.Beep(37, 50);
+            Console.Beep(37, 50);
+            Console.Beep(294, 100);
+            Console.Beep(294, 50);
+            Console.Beep(329, 750);
+            Console.Beep(329, 250);
+            Console.Beep(523, 100);
+            Console.Beep(493, 100);
+            Console.Beep(440, 100);
+            Console.Beep(392, 100);
+            Console.Beep(587, 500);
+            Console.Beep(440, 250);
+            Console.Beep(37, 50);
+            Console.Beep(294, 100);
+            Console.Beep(294, 50);
+            //intro starwars end
+
+            //string tmp = "";
+            //int freq, duration;
+            //while ((tmp = Console.ReadLine()).Length > 0)
+            //{
+            //    freq = Int32.Parse(tmp);
+            //    duration = Int32.Parse(Console.ReadLine());
+            //    Console.Beep(freq, duration);
+
+
+            //    //bool isOk = Int32.TryParse(Console.ReadLine(), out num);
+            //    //if (isOk)
+            //    //{
+            //    //    Console.WriteLine($"ok: {num}");
+            //    //}
+            //    //else
+            //    //{
+            //    //    Console.WriteLine("nie udało się");
+            //    //}
+            //}
+        }
+    }
+}
