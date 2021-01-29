e14abf205cf536c23de584868ea0b59ede292c93
MikeStork <44142875+MikeStork@users.noreply.github.com>
2021-01-29 15:41:32 +0100

    aaaa

---
+++ program5/program5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace program5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wsk = 0;
+            int[] tab = new int[100];
+            int[] idx = new int[100];
+            Array.Fill<int>(idx, -1);
+            Random r = new Random();
+            for(int i = 0; i<tab.Length; i++)
+            {
+                tab[i] = r.Next(0, 100);
+            }
+            int szukane = Convert.ToInt32(Console.ReadLine());
+            //na piechotę
+            for(int j= 0; j < tab.Length; j++)
+            {
+                if (tab[j] == szukane)
+                {
+                    idx[wsk] = j;
+                    wsk++;
+                }
+            }
+        }
+    }
+}
---
+++ program6/program6/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace program6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //z1
+            //double dd;
+            //double[] tabdd = new double[6];
+
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    double.TryParse(Console.ReadLine(), out dd);
+            //    tabdd[i] = dd;
+            //}
+
+            //int j = 0;
+            //while (j < tabdd.Length)
+            //{
+            //    Console.WriteLine(tabdd[j]);
+            //    j++;
+            //}
+
+            //z2
+            //string str = "";
+            //string[] tabstr = new string[5];
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    tabstr[i] = Console.ReadLine();
+
+            //}
+            //for (int j = 0; j < 5; j++)
+            //{
+
+            //    Console.WriteLine(tabstr[j].Length);
+            //}
+            //z3
+            //int[] tabint = new int[10];
+            //Random r = new Random();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    tabint[i] = r.Next(1, 10);
+            //}
+
+            //int[] result = new int[10];
+            //result = Array.FindAll<int>(tabint, x => x > 5);
+            //foreach(int x in result)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            //z4
+             
+            int[] tab = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                tab[i] = r.Next(1, 10);
+            }
+            for(int j = 0; j<tab.Length; j++)
+            {
+                if (Array.FindAll<int>(tab, x => x == tab[j]).Length > tab.Length / 2)
+                {
+                    Console.WriteLine("MA");
+                }
+                   
+                        
+                
+                
+            }
+            
+        }
+    }
+}
