3e40684fdbd6fe256a2d0b2065fcfdf1d7ee4bed
MikeStork <44142875+MikeStork@users.noreply.github.com>
2021-02-05 15:43:46 +0100

    update

--- program 9/program 9/Program.cs
+++ program 9/program 9/Program.cs
@@ -9,29 +9,32 @@ namespace program_9
         static void Main(string[] args)
         {
             //z1
-            //Console.Title = "z3";
-            //List<char> spr = new List<char>();
-            //char a;
-            //while (char.TryParse(Console.ReadLine(), out a))
-            //{
-            //    spr.Add(a);
+            Console.Title = "z3";
+            List<char> spr = new List<char>();
+            char a;
+            while (char.TryParse(Console.ReadLine(), out a))
+            {
+                if (a != ' ')
+                {
+                    spr.Add(a);
+                }
 
-            //}
-            //List<char> spr2 = spr.ToList();
-            //spr2.Reverse();
-            //for(int i = 0; i<spr.Count; i++)
-            //{
-            //    if (spr[i] == spr2[i])
-            //    {
-            //        Console.WriteLine("ok  {0} : {1}", spr[i], spr2[i]);
+            }
+            List<char> spr2 = spr.ToList();
+            spr2.Reverse();
+            for (int i = 0; i < spr.Count; i++)
+            {
+                if (spr[i] == spr2[i])
+                {
+                    Console.WriteLine("ok  {0} : {1}", spr[i], spr2[i]);
 
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("nie ok  {0} : {1}", spr[i], spr2[i]);
-            //        return;
-            //    }
-            //}
+                }
+                else
+                {
+                    Console.WriteLine("nie ok  {0} : {1}", spr[i], spr2[i]);
+                    return;
+                }
+            }
             //z1
 
             //z2
@@ -83,33 +86,33 @@ namespace program_9
             //3
 
             //4
-            List<int[]> pole = new List<int[]>();
-            //int[] tab = new int[] {10,2 };
-            //int[] tab2 = new int[] { 30, 12 };
-            //int[] tab3 = new int[] { 130, 21 };
-            //int[] tab4 = new int[] { 12, 23 };
-            Random r = new Random();
-            for(int k = 0; k<5; k++)
-            {
-                pole.Add(new int[2]{r.Next(1,50), r.Next(1, 50)});
-            }
-            //pole.Add(tab); pole.Add(tab2); pole.Add(tab3); pole.Add(tab4);
-            int najwpol = 0;
-            int idx = -1;
-            for (int ii = 0; ii < pole.Count; ii++) {
-                int tmp = pole[ii][0] * pole[ii][1];
-                if (tmp > najwpol)
-                {
-                    najwpol = tmp;
-                    idx = ii;
-                }
-            }
-            Console.BackgroundColor = ConsoleColor.DarkGreen;
-            Console.Clear();
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Title = "z2";
+            //List<int[]> pole = new List<int[]>();
+            ////int[] tab = new int[] {10,2 };
+            ////int[] tab2 = new int[] { 30, 12 };
+            ////int[] tab3 = new int[] { 130, 21 };
+            ////int[] tab4 = new int[] { 12, 23 };
+            //Random r = new Random();
+            //for(int k = 0; k<5; k++)
+            //{
+            //    pole.Add(new int[2]{r.Next(1,50), r.Next(1, 50)});
+            //}
+            ////pole.Add(tab); pole.Add(tab2); pole.Add(tab3); pole.Add(tab4);
+            //int najwpol = 0;
+            //int idx = -1;
+            //for (int ii = 0; ii < pole.Count; ii++) {
+            //    int tmp = pole[ii][0] * pole[ii][1];
+            //    if (tmp > najwpol)
+            //    {
+            //        najwpol = tmp;
+            //        idx = ii;
+            //    }
+            //}
+            //Console.BackgroundColor = ConsoleColor.DarkGreen;
+            //Console.Clear();
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.Title = "z2";
 
-            Console.WriteLine("\tNajw pole: {0}\t Tablica pod indexem: {1}\t Wartości: {2} i {3}", najwpol, idx, pole[idx][0], pole[idx][1]);
+            //Console.WriteLine("\tNajw pole: {0}\t Tablica pod indexem: {1}\t Wartości: {2} i {3}", najwpol, idx, pole[idx][0], pole[idx][1]);
             //4
         }
 
